b2b1f7850c7b36ba97c8caf6d7a0a89492f0b80f
gaurang007 <gaurangghadiya99@gmail.com>
2023-03-28 17:27:24 +0530

    tshirt page design

--- components/navbar.js
+++ components/navbar.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-    <header className="text-white bg-black body-font">
+    <header className="text-white bg-black body-font shadow-md">
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <Link href={"/"} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
       {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
@@ -13,7 +13,7 @@ const Navbar = () => {
       <span className="text-white text-xl">Tailblocks</span>
     </Link>
     <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-      <Link href={"/"} className="mr-5 hover:text-white">First Link</Link>
+      <Link href={"/tshirts"} className="mr-5 hover:text-white">Tshirts</Link>
       <Link href={"/"} className="mr-5 hover:text-white">Second Link</Link>
       <Link href={"/"} className="mr-5 hover:text-white">Third Link</Link>
       <Link href={"/"} className="mr-5 hover:text-white">Fourth Link</Link>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
-
 
 export default function Home() {
   return (
@@ -13,7 +10,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
       <main>
         <div style={{ width: "100%", height: "90vh", position: "relative" }}>
           <Image
@@ -140,7 +136,6 @@ export default function Home() {
           </div>
         </section>
       </main>
-      <Footer />
     </>
   );
 }
